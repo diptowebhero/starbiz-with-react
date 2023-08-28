5f8dbc3c43437b581b58e11a6b07d72495ae975f
diptowebhero <diptodas1672@gmail.com>
2023-08-28 23:31:28 +0600

    feat: our work section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import Banner from "../components/Banner/Banner";
+import OurWorks from "../components/OurWorks/OurWorks";
 
 export default function Home() {
   return (
     <>
       <Banner />
-
+      <OurWorks />
     </>
   );
 }
