8d8652441771873401704d18436409078d2c59ba
arunabhnewar <newardevil@gmail.com>
2023-09-04 20:25:23 +0600

    feat: team section add in about page

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -1,9 +1,44 @@
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+import OwlCarousel from "react-owl-carousel";
 import { Link } from "react-router-dom";
 import team01 from "../../assets/images/team/01.jpg";
 import team02 from "../../assets/images/team/02.jpg";
 import team03 from "../../assets/images/team/03.jpg";
 import team04 from "../../assets/images/team/04.jpg";
 import team05 from "../../assets/images/team/05.jpg";
+
+const options = {
+  loop: true,
+  center: true,
+  items: 3,
+  margin: 30,
+  autoplay: true,
+  // dots: true,
+  // dotsEach: false,
+  autoplayTimeout: 8500,
+  smartSpeed: 450,
+  nav: true,
+  navText: [
+    '<span class="las la-long-arrow-alt-left"><span>',
+    '<span class="las la-long-arrow-alt-right"></span>',
+  ],
+
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    768: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+};
 
 export default function Team() {
   return (
@@ -20,18 +55,12 @@ export default function Team() {
           </div>
           <div className='row'>
             <div className='col'>
-              <div
-                className='owl-carousel'
-                data-dots='false'
-                data-nav='true'
-                data-items='4'
-                data-xxl-items='4'
-                data-xl-items='4'
-                data-lg-items='3'
-                data-md-items='2'
-                data-sm-items='1'
-                data-margin='30'
-                data-autoplay='true'>
+              <OwlCarousel
+                className='owl-carousel owl-theme'
+                loop
+                margin={10}
+                nav
+                {...options}>
                 <div className='item'>
                   <div className='team-member'>
                     <h4>
@@ -172,7 +201,7 @@ export default function Team() {
                     </div>
                   </div>
                 </div>
-              </div>
+              </OwlCarousel>
             </div>
           </div>
         </div>
