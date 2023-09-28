0050a605c8f033815224ca6e9f7bab80d6e87a97
ferdousalam007 <iamferdousalam007@gmail.com>
2023-09-28 17:51:01 +0600

    feat: previous work  testimonial  slider add

--- src/components/PrevWork/PrevWork.jsx
+++ src/components/PrevWork/PrevWork.jsx
@@ -5,185 +5,192 @@ import portfolioImg3 from "../../assets/images/portfolio/08.jpg";
 import portfolioImg4 from "../../assets/images/portfolio/09.jpg";
 import portfolioImg5 from "../../assets/images/portfolio/10.jpg";
 
-import "owl.carousel/dist/assets/owl.carousel.css";
-import "owl.carousel/dist/assets/owl.theme.default.css";
-import OwlCarousel from "react-owl-carousel";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay, Pagination } from "swiper/modules";
 
-//Owl Carousel Settings
-const options = {
-  loop: true,
-  center: true,
-  items: 3,
-  margin: 30,
-  autoplay: true,
-  dots: true,
-  dotsEach: false,
-  autoplayTimeout: 8500,
-  smartSpeed: 450,
-  nav: false,
-  navText: [
-    '<span class="las la-long-arrow-alt-left"><span>',
-    '<span class="las la-long-arrow-alt-right"></span>',
-  ],
+import "swiper/css";
+import "swiper/css/navigation";
 
-  responsive: {
-    0: {
-      items: 1,
-    },
-    576: {
-      items: 2,
-    },
-    768: {
-      items: 2,
-    },
-    1024: {
-      items: 3,
-    },
-    1200: {
-      items: 4,
-    },
-  },
-};
 
 export default function PrevWork() {
   return (
     <>
-      <section className='pt-0'>
-        <div className='container-fluid'>
-          <div className='row align-items-center'>
-            <div className='col-lg-3 text-center'>
-              <div className='round-text'>
-                <svg
-                  version='1.1'
-                  xmlns='http://www.w3.org/2000/svg'
-                  x='0px'
-                  y='0px'
-                  width='100%'
-                  height='100%'
-                  viewBox='0 0 180 180'
-                  enableBackground='new 0 0 180 180'
-                  xmlSpace='preserve'>
-                  <defs>
-                    <path
-                      id='circlePath'
-                      d='M 90, 90 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 '></path>
-                  </defs>
-                  <circle cx='90' cy='90' r='75' fill='none'></circle>
-                  <g>
-                    <use xlinkHref='#circlePath' fill='none'></use>
-                    <text>
-                      <textPath xlinkHref='#circlePath'>
-                        Since 1980 • Since 1980 • Since 1980 •
-                      </textPath>
-                    </text>
-                  </g>
-                </svg>
-                <div className='text-btn-icon'>
-                  <i aria-hidden='true' className='flaticon flaticon-award'></i>
+      
+        <section className="pt-0">
+          <div className="container-fluid">
+            <div className="row align-items-center">
+              <div className="col-lg-3 text-center">
+                <div className="round-text">
+                  <svg
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    x="0px"
+                    y="0px"
+                    width="100%"
+                    height="100%"
+                    viewBox="0 0 180 180"
+                    enableBackground="new 0 0 180 180"
+                    xmlSpace="preserve"
+                  >
+                    <defs>
+                      <path
+                        id="circlePath"
+                        d="M 90, 90 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 "
+                      ></path>
+                    </defs>
+                    <circle cx="90" cy="90" r="75" fill="none"></circle>
+                    <g>
+                      <use xlinkHref="#circlePath" fill="none"></use>
+                      <text>
+                        <textPath xlinkHref="#circlePath">
+                          Since 1980 • Since 1980 • Since 1980 •
+                        </textPath>
+                      </text>
+                    </g>
+                  </svg>
+                  <div className="text-btn-icon">
+                    <i
+                      aria-hidden="true"
+                      className="flaticon flaticon-award"
+                    ></i>
+                  </div>
+                </div>
+                <div className="theme-title">
+                  <h2>We’ve Done Lot’s of Work</h2>
                 </div>
               </div>
-              <div className='theme-title'>
-                <h2>We’ve Done Lot’s of Work</h2>
-              </div>
-            </div>
-            <div className='col-lg-9'>
-              <OwlCarousel
-                OwlCarousel
-                className='owl-carousel owl-theme'
-                loop
-                margin={10}
-                nav
-                {...options}>
-                <div className='item'>
-                  <div className='portfolio-item style-3'>
-                    <div className='portfolio-image'>
-                      <img src={portfolioImg} className='img-fluid' alt='' />
+              <div className="col-lg-9">
+              <Swiper
+        modules={[Navigation, Autoplay, Pagination]}
+        loop={true}
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 3000 }}
+        navigation
+        spaceBetween={50}
+        slidesPerView={3}
+        breakpoints={{
+          375: {
+            slidesPerView: 1,
+            spaceBetween: 15,
+          },
+          450: {
+            slidesPerView: 1,
+            spaceBetween: 15,
+          },
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 15,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 5,
+          },
+          1024: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          1100: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          },
+        }}
+        // onSlideChange={() => console.log("slide change")}
+        // onSwiper={(swiper) => console.log(swiper)}
+      >
+              <SwiperSlide><div className="item">
+                  <div className="portfolio-item style-3">
+                    <div className="portfolio-image">
+                      <img src={portfolioImg} className="img-fluid" alt="" />
                     </div>
-                    <div className='portfolio-desc'>
-                      <div className='portfolio-cat'>
-                        <Link to='/service/3' rel='tag'>
+                    <div className="portfolio-desc">
+                      <div className="portfolio-cat">
+                        <Link to="/service/3" rel="tag">
                           Development
                         </Link>
                       </div>
                       <h4>
-                        <Link to='/service/3'>Web Development</Link>
+                        <Link to="/service/3">Web Development</Link>
                       </h4>
                     </div>
                   </div>
-                </div>
-                <div className='item'>
-                  <div className='portfolio-item style-3'>
-                    <div className='portfolio-image'>
-                      <img src={portfolioImg2} className='img-fluid' alt='' />
+                </div></SwiperSlide>
+              <SwiperSlide><div className="item">
+                  <div className="portfolio-item style-3">
+                    <div className="portfolio-image">
+                      <img src={portfolioImg2} className="img-fluid" alt="" />
                     </div>
-                    <div className='portfolio-desc'>
-                      <div className='portfolio-cat'>
-                        <Link to='/service/2' rel='tag'>
+                    <div className="portfolio-desc">
+                      <div className="portfolio-cat">
+                        <Link to="/service/2" rel="tag">
                           Development
                         </Link>
                       </div>
                       <h4>
-                        <Link to='/service/2'>Mobile App</Link>
+                        <Link to="/service/2">Mobile App</Link>
                       </h4>
                     </div>
                   </div>
-                </div>
-                <div className='item'>
-                  <div className='portfolio-item style-3'>
-                    <div className='portfolio-image'>
-                      <img src={portfolioImg3} className='img-fluid' alt='' />
+                </div></SwiperSlide>
+              <SwiperSlide><div className="item">
+                  <div className="portfolio-item style-3">
+                    <div className="portfolio-image">
+                      <img src={portfolioImg3} className="img-fluid" alt="" />
                     </div>
-                    <div className='portfolio-desc'>
-                      <div className='portfolio-cat'>
-                        <Link to='/service/1' rel='tag'>
+                    <div className="portfolio-desc">
+                      <div className="portfolio-cat">
+                        <Link to="/service/1" rel="tag">
                           Design
                         </Link>
                       </div>
                       <h4>
-                        <Link to='/service/1'>UI/UX Experience</Link>
+                        <Link to="/service/1">UI/UX Experience</Link>
                       </h4>
                     </div>
                   </div>
-                </div>
-                <div className='item'>
-                  <div className='portfolio-item style-3'>
-                    <div className='portfolio-image'>
-                      <img src={portfolioImg4} className='img-fluid' alt='' />
+                </div></SwiperSlide>
+              <SwiperSlide><div className="item">
+                  <div className="portfolio-item style-3">
+                    <div className="portfolio-image">
+                      <img src={portfolioImg4} className="img-fluid" alt="" />
                     </div>
-                    <div className='portfolio-desc'>
-                      <div className='portfolio-cat'>
-                        <Link to='/service/5' rel='tag'>
+                    <div className="portfolio-desc">
+                      <div className="portfolio-cat">
+                        <Link to="/service/5" rel="tag">
                           Management
                         </Link>
                       </div>
                       <h4>
-                        <Link to='/service/5'>ERP System</Link>
+                        <Link to="/service/5">ERP System</Link>
                       </h4>
                     </div>
                   </div>
-                </div>
-                <div className='item'>
-                  <div className='portfolio-item style-3'>
-                    <div className='portfolio-image'>
-                      <img src={portfolioImg5} className='img-fluid' alt='' />
+                </div></SwiperSlide>
+              <SwiperSlide><div className="item">
+                  <div className="portfolio-item style-3">
+                    <div className="portfolio-image">
+                      <img src={portfolioImg5} className="img-fluid" alt="" />
                     </div>
-                    <div className='portfolio-desc'>
-                      <div className='portfolio-cat'>
-                        <Link to='/service/6' rel='tag'>
+                    <div className="portfolio-desc">
+                      <div className="portfolio-cat">
+                        <Link to="/service/6" rel="tag">
                           Management
                         </Link>
                       </div>
                       <h4>
-                        <Link to='/service/6'>CMS System</Link>
+                        <Link to="/service/6">CMS System</Link>
                       </h4>
                     </div>
                   </div>
-                </div>
-              </OwlCarousel>
+                </div></SwiperSlide>
+
+                </Swiper>
+                
+            
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      
     </>
   );
 }
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,115 +1,115 @@
 import testimonialImg2 from "../../assets/images/testimonial/002.jpg";
 import testimonialImg from "../../assets/images/testimonial/01.jpg";
-import OwlCarousel from "react-owl-carousel";
-import "owl.carousel/dist/assets/owl.carousel.css";
-import "owl.carousel/dist/assets/owl.theme.default.css";
 
-//Owl Carousel Settings
-const options = {
-  loop: true,
-  center: true,
-  items: 3,
-  margin: 30,
-  autoplay: true,
-  dots: false,
-  dotsEach: false,
-  autoplayTimeout: 8500,
-  smartSpeed: 450,
-  nav: true,
-  navText: [
-    '<span class="las la-long-arrow-alt-left"><span>',
-    '<span class="las la-long-arrow-alt-right"></span>',
-  ],
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay, Pagination } from "swiper/modules";
 
-  responsive: {
-    0: {
-      items: 1,
-    },
-    576: {
-      items: 1,
-    },
-    768: {
-      items: 1,
-    },
-    1024: {
-      items: 1,
-    },
-    1200: {
-      items: 1,
-    },
-  },
-};
+import "swiper/css";
+import "swiper/css/navigation";
+
 export default function Testimonials() {
   return (
     <>
-      <section className='bg-dark-2'>
-        <div className='container'>
-          <div className='row'>
-            <div className='col'>
-              <OwlCarousel
-                OwlCarousel
-                className='owl-carousel owl-theme no-pb'
-                loop
-                margin={10}
-                nav
-                {...options}
-                >
+      <section className="bg-dark-2">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <Swiper
+                modules={[Navigation, Autoplay, Pagination]}
+                loop={true}
+                pagination={{ clickable: true }}
+                autoplay={{ delay: 3000 }}
+                navigation
+                slidesPerView={1}
+                breakpoints={{
+                  375: {
+                    slidesPerView: 1,
+                    spaceBetween: 15,
+                  },
+                  450: {
+                    slidesPerView: 1,
+                    spaceBetween: 15,
+                  },
+                  640: {
+                    slidesPerView: 1,
+                    spaceBetween: 15,
+                  },
+                  768: {
+                    slidesPerView: 2,
+                    spaceBetween: 5,
+                  },
+                  1024: {
+                    slidesPerView: 1,
+                    spaceBetween: 20,
+                  },
+                  1100: {
+                    slidesPerView: 1,
+                    spaceBetween: 20,
+                  },
+                }}
+                // onSlideChange={() => console.log("slide change")}
+                // onSwiper={(swiper) => console.log(swiper)}
+              >
                 {" "}
-                <div className='item'>
-                  <div className='testimonial style-1'>
-                    <div className='testimonial-img'>
-                      <img
-                        className='img-fluid'
-                        src={testimonialImg}
-                        alt='Image'
-                      />
-                      <div className='testimonial-caption'>
-                        <h5>Steven White</h5>
-                        <span>CEO</span>
+                <SwiperSlide>
+                  <div className="item">
+                    <div className="testimonial style-1">
+                      <div className="testimonial-img">
+                        <img
+                          className="img-fluid"
+                          src={testimonialImg}
+                          alt="Image"
+                        />
+                        <div className="testimonial-caption">
+                          <h5>Steven White</h5>
+                          <span>CEO</span>
+                        </div>
                       </div>
-                    </div>
-                    <div className='testimonial-content'>
-                      <div className='testimonial-quote'>
-                        <i className='flaticon flaticon-quotes'></i>
+                      <div className="testimonial-content">
+                        <div className="testimonial-quote">
+                          <i className="flaticon flaticon-quotes"></i>
+                        </div>
+                        <p>
+                          Look no further, this is the company to go with. What
+                          a pleasure it is to deal with friendly, professional
+                          individuals who take pride in their work. I highly
+                          recommend PixelFusion Look no further, this is the
+                          company to go with.
+                        </p>
                       </div>
-                      <p>
-                        Look no further, this is the company to go with. What a
-                        pleasure it is to deal with friendly, professional
-                        individuals who take pride in their work. I highly
-                        recommend PixelFusion Look no further, this is the
-                        company to go with.
-                      </p>
                     </div>
                   </div>
-                </div>
-                <div className='item'>
-                  <div className='testimonial style-1'>
-                    <div className='testimonial-img'>
-                      <img
-                        className='img-fluid'
-                        src={testimonialImg2}
-                        alt='Image'
-                      />
-                      <div className='testimonial-caption'>
-                        <h5>Maria Bella</h5>
-                        <span>Manager</span>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className="item">
+                    <div className="testimonial style-1">
+                      <div className="testimonial-img">
+                        <img
+                          className="img-fluid"
+                          src={testimonialImg2}
+                          alt="Image"
+                        />
+                        <div className="testimonial-caption">
+                          <h5>Maria Bella</h5>
+                          <span>Manager</span>
+                        </div>
                       </div>
-                    </div>
-                    <div className='testimonial-content'>
-                      <div className='testimonial-quote'>
-                        <i className='flaticon flaticon-quotes'></i>
+                      <div className="testimonial-content">
+                        <div className="testimonial-quote">
+                          <i className="flaticon flaticon-quotes"></i>
+                        </div>
+                        <p>
+                          Look no further, this is the company to go with. What
+                          a pleasure it is to deal with friendly, professional
+                          individuals who take pride in their work. I highly
+                          recommend PixelFusion Look no further, this is the
+                          company to go with.
+                        </p>
                       </div>
-                      <p>
-                        Look no further, this is the company to go with. What a
-                        pleasure it is to deal with friendly, professional
-                        individuals who take pride in their work. I highly
-                        recommend PixelFusion Look no further, this is the
-                        company to go with.
-                      </p>
                     </div>
                   </div>
-                </div>
-              </OwlCarousel>
+                </SwiperSlide>
+              </Swiper>
             </div>
           </div>
         </div>
